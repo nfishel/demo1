4e9a46a362d80c1db36d5af17fd8f5a02ec6c53b
Mr. Fishel <nfishel@hse.k12.in.us>
2020-11-03 10:55:04 -0500

    updated main.js and added style

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
+//get h1 from the HTML
+const answer = document.querySelector('#answer');
 
+//get today's date
+const today = new Date();
+
+//this function checks to see if the current day is Friday
+function checkForFriday(currentDate){
+  if(currentDate.getDay() === 6){
+    answer.innerHTML = "YES :)"
+  } else {
+    answer.innerHTML = "NO :("
+  }
+} //end of checkForFriday
+
+checkForFriday(today);
